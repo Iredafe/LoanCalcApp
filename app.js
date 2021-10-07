eaf1caabc3b86e078e2955d35b3ae534182442ea
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-10-06 19:08:47 -0600

    create ui vars

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-//Lidten for submit
+//Listen for submit
 document.getElementById('loan-form').addEventListener('submit', calculateResults)
+
+
+function calculateResults(e){
+    //UI vars
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment')
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+    
+
+
+    e.preventDefault();
+}
