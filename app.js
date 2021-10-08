f09e2201792805cff6f524824a3f4d0230eb809e
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-10-07 19:51:50 -0600

    make loader show for 2 secs, closes #2

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 //Listen for submit
 document.getElementById('loan-form').addEventListener('submit', function(e){
 
-    //hide results
-    document.getElementById('results').style.display = 'none';
     //show loader
     document.getElementById('loading').style.display = 'block';
+
+    setTimeout(calculateResults,2000);
+
     e.preventDefault();
 })
 
@@ -31,6 +32,12 @@ function calculateResults(){
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+           //show results
+    document.getElementById('results').style.display = 'block';
+
+        //hide loader
+    document.getElementById('loading').style.display = 'none';
     }else {
             showError('Please check your numbers');
     }
